f4c7b7f54ab872ec202429e5eecb1a1451ccb0b5
aandreyev <andreyev.andrey@gmail.com>
2016-10-01 12:09:11 -0400

    finished problem 2

---
+++ knapsack/build/CMakeFiles/knap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/knap.dir/knapsack.cpp.o"
+  "knap.pdb"
+  "knap"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/knap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -3,20 +3,37 @@
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
-    }
-  }
-  return current_weight;
-}
+int optimal_weight(int Weight, const vector<int> &w) {
+	int length=w.size();
+	//initialize 2D Array
+	vector<vector<int>> value;
+	value.resize(length+1,vector<int>(Weight+1,0));
+	for (int i=0;i<=length;i++){
+		value[i][0]=0;
+	}
+	for (int j=0;j<=Weight;j++){
+		value[0][j]=0;
+	}
+	for (int i=1;i<=length;i++){
+		for(int j=1;j<=Weight;j++){
+			value[i][j]=value[i-1][j];
+			if(w[i-1]<=j){
+				int wi=w[i-1];
+				int val=value[i-1][j-wi]+wi;
+				if(value[i][j]<val) {
+					value[i][j]=val;
+				}
+			}
+		}
+	}
+	
+	return value[length][Weight];
+ }
 
 int main() {
   int n, W;
-  std::cin >> W >> n;
+  
+   std::cin >> W >> n;
   vector<int> w(n);
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
